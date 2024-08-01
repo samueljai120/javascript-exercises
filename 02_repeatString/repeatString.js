f9d650f5b05d63ff4cc14ae9c3ef60fe2685a74f
samueljai120 <173202232+samueljai120@users.noreply.github.com>
2024-07-31 20:02:40 -0400

    loop passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    let myString = "";
+    while (num > 0) {
+        myString += string;
+        num--;
+    }
+    return myString
 };
-
+repeatString('hey', 3);
 // Do not edit below this line
 module.exports = repeatString;
