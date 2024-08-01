550e29270bee7943e668a20c901d5e3cf07a7910
samueljai120 <173202232+samueljai120@users.noreply.github.com>
2024-07-31 21:47:41 -0400

    test done, inspired by ..arg and negate method

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,9 +1,10 @@
-const removeFromArray = function(array, target) {
-   return array.filter(num => num != target)
+const removeFromArray = function(array, ...target) {
+   return array.filter(val => !target.includes(val))
 };
 
 
 removeFromArray([1, 2, 3, 4], 3); // should remove 3 and return [1,2,4]
+removeFromArray([1, 2, 3, 4], 3, 2);
 
 // Do not edit below this line
 module.exports = removeFromArray;
