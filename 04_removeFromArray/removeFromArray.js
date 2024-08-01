8af9fffaab7d09ef427b50506a86d0c29972db75
samueljai120 <173202232+samueljai120@users.noreply.github.com>
2024-07-31 21:37:48 -0400

    done test 1, starting test 2 clue in ...arg

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, target) {
+   return array.filter(num => num != target)
 };
+
+
+removeFromArray([1, 2, 3, 4], 3); // should remove 3 and return [1,2,4]
 
 // Do not edit below this line
 module.exports = removeFromArray;
