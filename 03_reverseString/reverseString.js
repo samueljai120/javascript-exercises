0e8d1d331299ae0d1d5212fd67a41c06151098e8
samueljai120 <173202232+samueljai120@users.noreply.github.com>
2024-07-31 20:39:22 -0400

    use split, reverse and join method, deleted skip in test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
+const reverseString = function(greet) {
+    const str = greet.split('').reverse();
+    return str.join('')
 
 };
 
+reverseString('hello there')
 // Do not edit below this line
 module.exports = reverseString;
