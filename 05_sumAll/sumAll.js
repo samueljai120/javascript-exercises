4c7942a6edea2920abd2ce895432513991917077
samueljai120 <173202232+samueljai120@users.noreply.github.com>
2024-08-01 21:01:17 -0400

    passed 2 tests, working on third test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,15 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let finalSum = [];
+    for (let i = start; i <= end; i++) {
+        finalSum.push(i);
+    }
+    return finalSum.reduce((prev,curr) => prev + curr, 0)
 
 };
 
+
+
+
+sumAll(1, 4) // returns the sum of 1 + 2 + 3 + 4 which is 10
 // Do not edit below this line
 module.exports = sumAll;
