96b8ecc11248eb6dd97951784a4ae329054da876
samueljai120 <173202232+samueljai120@users.noreply.github.com>
2024-08-04 12:41:21 -0400

    learned how to use math.round, as well as how to rond up decimals by times ten when math round and divide 10 after math round

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheit) {
+  const celsius = (farenheit - 32) * 5/9;
+  return Math.round(celsius*10)/10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  const farenheit = celcius * 9/5 +32;
+  return Math.round(farenheit*10)/10
+
+
 };
 
 // Do not edit below this line
