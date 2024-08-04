2a41ab6857ae4ad686f0654b14bbbda4f57b7467
samueljai120 <173202232+samueljai120@users.noreply.github.com>
2024-08-04 14:23:53 -0400

    fun excercise as this requires to learn replace method to simplify the string into a more desirble environment to compare, as well as the tolowercase method, where the comparison of the string value itself is also interesting

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+    string = string.replace(/\W/, "");
+    string = string.toLowerCase();
+    for (let i = 0; i < string.length; i++){
+        if (string[i] != string[string.length - 1 - i]){
+            return false
+        }
+    }
+    return true
 };
 
 // Do not edit below this line
