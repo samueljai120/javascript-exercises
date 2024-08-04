0338232db13e12b5b6c5b6273d5d8d02c59159ce
samueljai120 <173202232+samueljai120@users.noreply.github.com>
2024-08-04 13:16:51 -0400

    the most challeging part would be factorial, as the factorial method is pretty new to me, as well as the recap of reduce method.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((prev,curr) => prev + curr, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((prev,curr) => (prev * curr))
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b)
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num >= 0 && num <= 1){
+    return 1;
+  } else {
+    return num * factorial(num-1);
+  }
+	// let factorial = [];
+  // while(num > 0){
+  //   factorial.push(num);
+  //   num--;
+  // }
+  // // for (let i = num, i > 0; i--){
+  // //   factorial.push(i);
+  // // }
+  // return multiply(factorial)
 };
 
 // Do not edit below this line
