166724a50d219f5e6fc52415dc619dfe912c3e23
samueljai120 <173202232+samueljai120@users.noreply.github.com>
2024-08-05 11:36:29 -0400

    this fibonacci sequence is challenging as it requires the thinking path of swapping values in an equation. as well as re declaring the n0 and n1 fibonacci numbers

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(count) {
+    if (count < 0) return'OOPS';
+    if (count == 0) return 0;
+    let fib1 = 1;
+    let fib2 = 0;
+
+    for (let i = 2; i <= count; i++){
+        let cur = fib1 + fib2;
+        fib2 = fib1;
+        fib1 = cur;
+        
+    }
+    return fib1
 
 };
 
